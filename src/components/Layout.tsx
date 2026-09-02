3b5b60adfebd23a195b539ec041e4ff45b16f9a4
sumitkumards07 <sumitkumards07@gmail.com>
2026-09-02 21:43:40 +0530

    Update phone number to 7850064448

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import { Phone, Mail, MapPin, Clock, Facebook, Twitter, Instagram, Youtube, Menu, X, MessageCircle } from 'lucide-react';
 
-export const PHONE_NUMBER = "919728178351";
+export const PHONE_NUMBER = "917850064448";
 
 export function Layout() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -111,7 +111,7 @@ export function Layout() {
                 </li>
                 <li className="flex items-center">
                   <Phone className="mr-3 h-5 w-5 flex-shrink-0 text-[#eab308]" />
-                  <a href={`tel:+${PHONE_NUMBER}`} className="hover:text-white transition">+91 9728178351</a>
+                  <a href={`tel:+${PHONE_NUMBER}`} className="hover:text-white transition">+91 7850064448</a>
                 </li>
                 <li className="flex items-center">
                   <Mail className="mr-3 h-5 w-5 flex-shrink-0 text-[#eab308]" />
